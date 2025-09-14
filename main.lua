d105525975c4b29c696c6cb4df420508802d437f
Phoenixymlv <phoenixymlv@hotmail.com>
2025-09-14 20:38:19 +0200

    Added main love funcs + empty config file

---
+++ main.lua
@@ -0,0 +1,54 @@
+---@diagnostic disable: undefined-global
+
+function love.load()
+    -- Initialization code here
+    
+end
+
+function love.update(dt)
+    -- Update game state here
+end
+
+function love.draw()
+    -- Draw everything here
+end
+
+function love.keypressed(key, scancode, isrepeat)
+    -- Handle key presses here
+end
+
+function love.keyreleased(key, scancode)
+    -- Handle key releases here
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    -- Handle mouse button presses here
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+    -- Handle mouse button releases here
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+    -- Handle mouse movement here
+end
+
+function love.wheelmoved(x, y)
+    -- Handle mouse wheel movement here
+end
+
+function love.textinput(text)
+    -- Handle text input here
+end
+
+function love.resize(w, h)
+    -- Handle window resize here
+end
+
+function love.focus(f)
+    -- Handle window focus/unfocus here
+end
+
+function love.quit()
+    -- Called when the game is quitting
+end
